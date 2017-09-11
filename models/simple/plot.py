abc106f4c25586483a0a27320a6f0221b5abd42b
hsartoris <hsartoris@gmail.com>
2017-09-10 22:36:02 -0400

    added gitignore for pngs, working plot function; network certainly doesn't die but doesnt necessarily function right either

--- models/simple/plot.py
+++ models/simple/plot.py
@@ -13,5 +13,4 @@ data = np.genfromtxt(sys.argv[1], delimiter=',')
 
 for i in range(0, len(data)):
 	plt.plot(data[i])
-
-plt.savefig(sys.argv[1][:4] + ".png", dpi=200)
+plt.savefig(sys.argv[1][:-4] + ".png", dpi=200)
