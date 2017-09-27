6c4b3cf745a616b2782f5c63745bbccdc40af0a0
Hayden Sartoris <hsartoris@gmail.com>
2017-09-26 20:49:34 -0400

    now with more correlation

--- models/simple/makeData.py
+++ models/simple/makeData.py
@@ -4,6 +4,17 @@ import sys
 import os
 from backports.shutil_get_terminal_size import get_terminal_size
 
+def correlation(prev, curr):
+    prev = np.array(prev.transpose())[0]
+    curr = np.array(curr.transpose())[0]
+    m = np.zeros(shape=(len(prev), len(prev)))
+    for i in range(len(prev)):
+        if prev[i] == 0: continue
+        for j in range(len(curr)):
+            if i == j or curr[j] == 0: continue
+            m[i][j] = prev[i]/curr[j]
+    return np.array([m])
+
 if __name__ == "__main__":
     loading = ["|", "/", "-", "\\", "*"]
     prefix = "data2"
@@ -26,19 +37,28 @@ if __name__ == "__main__":
     pFloor = np.vectorize(simple.probFloor)
     logistic = np.vectorize(simple.logisticf)
     count = 0
+
     for i in range(0, networks):
         w = np.matrix(np.genfromtxt(prefix + "/" + str(i) + "/w.csv", delimiter=','))
         s = np.matrix(pFloor(np.random.rand(len(w), 1), initSpikeChance))
         out = s
+        totalCorr = np.array([np.zeros(shape=(len(w), len(w)))])
+        correlations = np.array([np.zeros(shape=(len(w), len(w)))])
         for j in range(0, iterations):
+            prev = s
             s = w * s
             s = s + np.matrix(pFloor(np.random.rand(len(w),1), spikeChance))
             s = logistic(s, threshold)
             out = np.append(out, s, 1)
+            corr = correlation(prev, s)
+            correlations = np.append(correlations, corr, 0)
+            totalCorr += corr
             sys.stdout.write("\r{0}>".format(loading[(count/600)%len(loading)] + ("="*((cols-3)*i/networks))))
             sys.stdout.flush()
             count += 1
         np.savetxt(prefix + "/" + str(i) + "/raw.csv", out, delimiter=',')
+        np.savetxt(prefix + "/" + str(i) + "/correlations.csv", correlations.reshape(-1, correlations.shape[-1]), delimiter=',')
+        np.savetxt(prefix + "/" + str(i) + "/total.csv", totalCorr.reshape(-1, totalCorr.shape[-1]), delimiter=',')
     sys.stdout.write("\r{0}".format("="*cols))
     sys.stdout.flush()
     print("Done simulating")
