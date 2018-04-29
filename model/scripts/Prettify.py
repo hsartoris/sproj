37668b6ebbc87c87cf5e93d0c7516b606eb3379b
Hayden Sartoris <hsartoris@gmail.com>
2018-04-29 16:52:54 -0400

    oh

--- model/scripts/Prettify.py
+++ model/scripts/Prettify.py
@@ -2,18 +2,18 @@ import sys
 import subprocess
 
 class pretty(object):
-        def __init__(self, cols = None):
-                if cols is None:
-                    rows, columns = subprocess.check_output(['stty', 
-                        'size']).split()
-                    self.columns = int(columns)
-                else:
-                    self.columns = cols
+    def __init__(self, cols = None):
+        if cols is None:
+            rows, columns = subprocess.check_output(['stty', 
+                'size']).split()
+            self.columns = int(columns)
+        else:
+            self.columns = cols
 
-	def write(self, string):
-		sys.stdout.write(string)
-		sys.stdout.flush()
-	
-	def arrow(self, curr, maxLen):
-		self.write("-"*int(float(curr)*(self.columns-1)/maxLen) + ">\r")
+        def write(self, string):
+            sys.stdout.write(string)
+                sys.stdout.flush()
+
+        def arrow(self, curr, maxLen):
+            self.write("-"*int(float(curr)*(self.columns-1)/maxLen) + ">\r")
 
